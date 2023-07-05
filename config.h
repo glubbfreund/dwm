f775e7130ecbb3754a331323a01d9222db6d90d5
Oliver Leis <oliver.leis@gmail.com>
2023-07-05 08:56:33 +0200

    changed bar padding

--- config.h
+++ config.h
@@ -14,8 +14,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char font[]            = "DejaVuSansM Nerd Font 9";
-static const char dmenufont[]       = "DejaVuSansM Nerd Font:size=9";
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 4;        /* vertical padding for statusbar */
+static const char font[]            = "DejaVuSansM Nerd Font 8";
+static const char dmenufont[]       = "DejaVuSansM Nerd Font:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
