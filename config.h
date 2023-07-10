0d277b39841a25823bc4fdf439f0cb56907d17dc
Oliver Leis <oliver.leis@gmail.com>
2023-07-10 20:47:54 +0200

    minor styling changes

--- config.h
+++ config.h
@@ -22,13 +22,13 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#31738d";
 static const char col_blue[]        = "#83cafa";
 static const char col_green[]        = "#9ec600";
 static const char *colors[][4]      = {
 	/*               fg             bg        border   */
 	[SchemeNorm] = { col_gray3,   col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4,   col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1,   col_cyan,  col_cyan  },
 	[SchemeMark]  = { col_green,   col_gray1,  col_green  },
 	[SchemeTitle]  = { col_blue, col_gray1,  col_cyan  },
 };
