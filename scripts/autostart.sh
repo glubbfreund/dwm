61f03568993bcc8863cb5c2cd1c36894c26ea1ec
Oliver Leis <oliver.leis@gmail.com>
2023-07-10 15:56:49 +0200

    refreshed the scripts

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ~/.fehbg &
+xrandr --auto --output DP-2 --mode 1920x1080 --right-of DP-0
 /usr/libexec/xfce4/notifyd/xfce4-notifyd &
 picom -b &
 ~/Src/dwm/scripts/sched &
@@ -10,4 +11,3 @@ udiskie -t &
 blueman-applet &
 /usr/libexec/polkit-gnome-authentication-agent-1 &
 setxkbmap -layout de,gr -option caps:escape
-xrandr --auto --output DP-2 --mode 1920x1080 --right-of DP-0
