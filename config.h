187c7512efd3614a0fb6484897b8ef64dd5197f7
glubbfreund <oliver.leis@gmail.com>
2023-11-20 18:20:59 +0100

    regenerated config.def.h and changed color

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char col_gray1[]       = "#1c1c1c";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_highlight[]   = "#B36200";
+static const char col_highlight[]   = "#984400";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
