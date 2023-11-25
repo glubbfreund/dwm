8fe5d1732d8e4110f3b9b4f4c8dd280740d17124
Glubbfreund <oliver.leis@gmail.com>
2023-11-25 10:21:18 +0100

    added maximum numer of masters

--- config.h
+++ config.h
@@ -14,6 +14,7 @@ static const int warpmouse                  = 1;        /* 1 means warp mouse to
 static const int swallowfloating            = 0;        /* 1 means swallow floating windows by default */
 static const int showbar                    = 1;        /* 0 means no bar */
 static const int topbar                     = 1;        /* 0 means bottom bar */
+static const int nmaxmaster                 = 2;        /* maximum number of clients allowed in master area */
 static const int showsystray                = 1;        /* 0 means no systray */
 static const int colortitle                 = 0;        /* 0 means no colored title in bar */
 static const int showtitle                  = 0;        /* 0 means no title */
