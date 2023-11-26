bf4c2a1ecb5e62488a184c533bba8959d8970a13
Glubbfreund <oliver.leis@gmail.com>
2023-11-26 23:07:10 +0100

    style change

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@
 #include "../colors/colors.h"
 
 /* appearance */
-static const unsigned int borderpx          = 2;        /* border pixel of windows */
+static const unsigned int borderpx          = 3;        /* border pixel of windows */
 static const unsigned int snap              = 32;       /* snap pixel */
-static const unsigned int gappx             = 10;       /* gaps between windows */
+static const unsigned int gappx             = 15;       /* gaps between windows */
 static const unsigned int systraypinning    = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft     = 1;        /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing    = 2;        /* systray spacing */
@@ -16,8 +16,8 @@ static const int showbar                    = 1;        /* 0 means no bar */
 static const int topbar                     = 1;        /* 0 means bottom bar */
 static const int nmaxmaster                 = 2;        /* maximum number of clients allowed in master area */
 static const int showsystray                = 1;        /* 0 means no systray */
-static const int colortitle                 = 0;        /* 0 means no colored title in bar */
-static const int showtitle                  = 0;        /* 0 means no title */
+static const int colortitle                 = 1;        /* 0 means no colored title in bar */
+static const int showtitle                  = 1;        /* 0 means no title */
 
 static const char *fonts[]                  = { "JetBrainsMono Nerd Font Mono:size=12" };
 static const char dmenufont[]               =   "JetBrainsMono Nerd Font Mono:size=12";
