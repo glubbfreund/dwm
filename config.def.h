27045c70eaa92c55b5c1ff459865bbdf300f29a8
Glubbfreund <oliver.leis@gmail.com>
2023-11-27 15:41:36 +0100

    removed windowmap patch and added awesomebar (awesomebar does mapping instead)

--- config.def.h
+++ config.def.h
@@ -16,8 +16,6 @@ static const int showbar                    = 1;        /* 0 means no bar */
 static const int topbar                     = 1;        /* 0 means bottom bar */
 static const int nmaxmaster                 = 2;        /* maximum number of clients allowed in master area */
 static const int showsystray                = 1;        /* 0 means no systray */
-static const int colortitle                 = 1;        /* 0 means no colored title in bar */
-static const int showtitle                  = 1;        /* 0 means no title */
 
 static const char *fonts[]                  = { "JetBrainsMono Nerd Font Mono:size=12" };
 static const char dmenufont[]               =   "JetBrainsMono Nerd Font Mono:size=12";
@@ -28,10 +26,12 @@ static const char col_gray3[]               = "#bbbbbb";
 static const char col_gray4[]               = "#eeeeee";
 
 static const char *colors[][3]              = {
-/*                        fg                bg              border      */
-	[SchemeNorm] =      { col_gray3,        col_gray1,      col_gray1   },
-	[SchemeSel]  =      { col_gray4,        col_highlight,  col_strong  },
-	[SchemeHighlight] = { col_highlight,    col_gray1,      col_gray2   }, 
+/*                          fg                  bg              border          */
+	[SchemeNorm]        =   { col_gray3,        col_gray1,      col_gray1       },
+	[SchemeSel]         =   { col_gray4,        col_highlight,  col_strong      },
+	[SchemeHighlight]   =   { col_highlight,    col_gray1,      col_gray2       }, 
+	[SchemeHov]         =   { col_gray4,        col_highlight,  col_highlight   },
+	[SchemeHid]         =   { col_highlight,    col_gray1,      col_highlight   },
 };
 
 static const Rule rules[] = {
@@ -95,8 +95,10 @@ static const Key keys[] = {
 	{ MODKEY,               XK_s,                       spawn,          {.v = dmenumobile       } },
 	{ MODKEY,               XK_e,                       spawn,          {.v = filemanager       } },
 	{ MODKEY,               XK_b,                       spawn,          {.v = browser           } },
-	{ MODKEY,               XK_j,                       focusstack,     {.i = +1 } },
-	{ MODKEY,               XK_k,                       focusstack,     {.i = -1 } },
+	{ MODKEY,               XK_j,                       focusstackvis,  {.i = +1 } },
+	{ MODKEY,               XK_k,                       focusstackvis,  {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_j,                       focusstackhid,  {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_k,                       focusstackhid,  {.i = -1 } },
 	{ MODKEY|ShiftMask,     XK_i,                       incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,     XK_d,                       incnmaster,     {.i = -1 } },
 	{ MODKEY,               XK_h,                       setmfact,       {.f = -0.05} },
@@ -116,6 +118,9 @@ static const Key keys[] = {
 	{ MODKEY,               XK_period,                  focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,     XK_comma,                   tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,     XK_period,                  tagmon,         {.i = +1 } },
+	{ MODKEY,               XK_s,                       show,           {0} },
+	{ MODKEY|ShiftMask,     XK_s,                       showall,        {0} },
+	{ MODKEY,               XK_h,                       hide,           {0} },
 	TAGKEYS(                XK_1,                       0)
 	TAGKEYS(                XK_2,                       1)
 	TAGKEYS(                XK_3,                       2)
@@ -137,6 +142,7 @@ static const Key keys[] = {
 static const Button buttons[] = {
 	/*click                 event mask          button          function        argument */
 	{ ClkLtSymbol,          0,                  Button1,        setlayout,      {0} },
+	{ ClkWinTitle,          0,                  Button1,        togglewin,      {0} },
 	{ ClkWinTitle,          0,                  Button3,        zoom,           {0} },
 	{ ClkLtSymbol,          0,                  Button3,        setlayout,      {.v = &layouts[1]} },
 	{ ClkStatusText,        0,                  Button2,        spawn,          {.v = termcmd } },
