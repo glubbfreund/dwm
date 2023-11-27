2d4563e3739e155365ba38a5be098bf5e378a06f
Glubbfreund <oliver.leis@gmail.com>
2023-11-27 10:53:51 +0100

    changed mouse bindings

--- config.def.h
+++ config.def.h
@@ -137,8 +137,8 @@ static const Key keys[] = {
 static const Button buttons[] = {
 	/*click                 event mask          button          function        argument */
 	{ ClkLtSymbol,          0,                  Button1,        setlayout,      {0} },
-	{ ClkWinTitle,          0,                  Button2,        zoom,           {0} },
-	{ ClkLtSymbol,          0,                  Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,                  Button3,        zoom,           {0} },
+	{ ClkLtSymbol,          0,                  Button3,        setlayout,      {.v = &layouts[1]} },
 	{ ClkStatusText,        0,                  Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,             Button1,        moveorplace,    {.i = 1} },
 	{ ClkClientWin,         MODKEY|ShiftMask,   Button1,        movemouse,      {0} },
